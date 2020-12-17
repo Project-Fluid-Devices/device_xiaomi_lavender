6f58bfc0efef3d37d1b96d89205162b1f23fb31c
DillerOFire <niktofe1@gmail.com>
2020-12-17 18:59:58 +1000

    lavender: Fluid-fy
    Signed-off-by: DillerOFire <niktofe1@gmail.com>

--- fluid_lavender.mk
+++ fluid_lavender.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,8 +28,10 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_lavender
+PRODUCT_NAME := fluid_lavender
 PRODUCT_MODEL := Redmi Note 7
+IS_PHONE := true
+FLUID_BUILD_TYPE := OFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -39,3 +41,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="lavender-user 10 QKQ1.190910.002 V12.0.1.0.QFGMIXM release-keys"
 
 BUILD_FINGERPRINT := xiaomi/lavender/lavender:10/QKQ1.190910.002/V12.0.1.0.QFGMIXM:user/release-keys
+
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.fluid.maintainer=Sfier Reichenbach \
+  ro.fluid.cpu=SDM660
