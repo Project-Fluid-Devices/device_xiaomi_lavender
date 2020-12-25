4d5abadae5696f1bc08535d84da1c94fbc1a89ab
DillerOFire <niktofe1@gmail.com>
2020-12-26 07:49:46 +1000

    lavender: Make gapps optional

--- fluid_lavender.mk
+++ fluid_lavender.mk
@@ -25,6 +25,14 @@ $(call inherit-product, vendor/fluid/config/common_full_phone.mk)
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+
+ifeq ($(WITH_GAPPS),true)
+TARGET_INCLUDE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_GAPPS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+endif
+
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
