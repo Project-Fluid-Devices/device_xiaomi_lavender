979261b3a181ae3b29850882d1e1b41d2c202f73
Mathieu Chartier <mathieuc@google.com>
2021-11-02 21:34:43 +0530

    lavender: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- device.mk
+++ device.mk
@@ -60,6 +60,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
